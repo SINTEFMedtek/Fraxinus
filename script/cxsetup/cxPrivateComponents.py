507fb9b433c0556d739270bf8d37b6a7a1379611
Ole Vegard Solberg <olevegard.solberg@sintef.no>
2022-05-06 13:36:37 +0200

    CX-490: Fixed fraxinus.tracking plugin

--- script/cxsetup/cxPrivateComponents.py
+++ script/cxsetup/cxPrivateComponents.py
@@ -91,7 +91,7 @@ class org_custusx_fraxinus_tracking(cx.build.cxComponents.CppComponent):
         return 'ssh://git@git.code.sintef.no/mt/org.custusx.fraxinus.tracking.git'
     def update(self):
         self._getBuilder().gitSetRemoteURL(self.repository())
-        self._getBuilder().gitCheckoutSha('3feefed401d814491f70b483eceaab29401ffdfc')
+        self._getBuilder().gitCheckoutSha('acaec427ed3ddb6a86879a09919f96ba33417339')
         #self._getBuilder().gitCheckoutDefaultBranch()
     def configure(self):
         pass
