dacd85145e8a26f78c02cb50047cc325637e09b6
Erlend Fagertun Hofstad <ehof@sintef.no>
2023-06-05 11:16:25 +0200

    FX-120: Removed keyboard control in VB workflows.

--- org.custusx.fraxinus.core.state/cxFraxinusWorkflowStates.cpp
+++ org.custusx.fraxinus.core.state/cxFraxinusWorkflowStates.cpp
@@ -552,7 +552,7 @@ void FraxinusWorkflowState::setupVBWidget(int flyThrough3DViewGroupNumber, int s
 	if(services)
 		services->view()->zoomCamera3D(flyThrough3DViewGroupNumber, VB3DCameraZoomSetting::getZoomFactor());
 	
-	this->getVBWidget()->grabKeyboard(); //NB! This make this widget take all keyboard input. E.g. "R" doesn't work in this workflow step.
+	//this->getVBWidget()->grabKeyboard(); //NB! This make this widget take all keyboard input. E.g. "R" doesn't work in this workflow step.
 	//Actually, "R" seems to be a special case since it is from VTK. Other key input might work, but maybe not if the menu bar is off.
 	//this->getVBWidget()->setFocus(); // Can't seem to get any affect from this regarding key input.
 }
@@ -628,7 +628,7 @@ void FraxinusWorkflowState::setMeshOpacity(MeshPtr mesh, double opacity)
 
 void FraxinusWorkflowState::cleanupVBWidget()
 {
-	this->getVBWidget()->releaseKeyboard();
+	//this->getVBWidget()->releaseKeyboard();
 	//this->getVBWidget()->clearFocus();
 }
 
@@ -1097,7 +1097,7 @@ void VirtualBronchoscopyFlyThroughWorkflowState::onEntry(QEvent * event)
 
 void VirtualBronchoscopyFlyThroughWorkflowState::onExit(QEvent * event)
 {
-	this->cleanupVBWidget();
+	//this->cleanupVBWidget();
 	WorkflowState::onExit(event);
 }
 
@@ -1205,7 +1205,7 @@ void VirtualBronchoscopyCutPlanesWorkflowState::onEntry(QEvent * event)
 
 void VirtualBronchoscopyCutPlanesWorkflowState::onExit(QEvent *event)
 {
-	this->cleanupVBWidget();
+	//this->cleanupVBWidget();
 	WorkflowState::onExit(event);
 }
 
@@ -1323,7 +1323,7 @@ void VirtualBronchoscopyAnyplaneWorkflowState::onEntry(QEvent * event)
 
 void VirtualBronchoscopyAnyplaneWorkflowState::onExit(QEvent * event)
 {
-	this->cleanupVBWidget();
+	//this->cleanupVBWidget();
 	WorkflowState::onExit(event);
 }
 
