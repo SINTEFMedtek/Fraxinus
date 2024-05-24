e8ba213bfc31e741c70c0fa5391de7720b4b95cd
Erlend Fagertun Hofstad <ehof@sintef.no>
2024-05-24 13:39:20 +0200

    Updated sha to org.custusx.fraxinus.tracking. Fraxinus#12

--- script/cxsetup/cxPrivateComponents.py
+++ script/cxsetup/cxPrivateComponents.py
@@ -89,7 +89,7 @@ class org_custusx_fraxinus_tracking(cx.build.cxComponents.CppComponent):
         return '%s/org.custusx.fraxinus.tracking.git' % self.controlData.gitrepo_main_site_base
     def update(self):
         self._getBuilder().gitSetRemoteURL(self.repository())
-        self._getBuilder().gitCheckoutSha('fcb48e6278a52c0b199cb707ffef5ace7f1aa2de')
+        self._getBuilder().gitCheckoutSha('538b6c4b17f73297c160f2ca61ae8c554f2a42bb')
         #self._getBuilder().gitCheckoutDefaultBranch()
     def configure(self):
         pass
