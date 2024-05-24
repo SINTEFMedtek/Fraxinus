85a0f0b18c579366f4825ed50f77f44baf4c4d63
Erlend Fagertun Hofstad <ehof@sintef.no>
2024-05-24 10:48:09 +0200

    Fixed typo. Fraxinus#12

--- org.custusx.fraxinus.core.state/cxFraxinusWorkflowStates.cpp
+++ org.custusx.fraxinus.core.state/cxFraxinusWorkflowStates.cpp
@@ -857,7 +857,7 @@ void ProcessWorkflowState::addDataToView()
 void ProcessWorkflowState::onExit(QEvent * event)
 {
 	//Set tooltip offset to 0 after loading patient.
-	//If Fraxinus is closed then EBUS simulator is running the offset used in the simulator will remain in the patient file.
+	//If Fraxinus is closed when EBUS simulator is running the offset used in the simulator will remain in the patient file.
 	ToolPtr tool = mServices->tracking()->getManualTool();
 	tool->setTooltipOffset(0);
 
