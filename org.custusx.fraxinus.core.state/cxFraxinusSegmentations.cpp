89744db0114ac1df543ee8f28eb8730ffc53c40e
Erlend Fagertun Hofstad <ehof@sintef.no>
2025-12-17 13:05:49 +0100

    Change to make sure Lungs are segmented before Airways, which is a necessity in Raidionics. Fraxinus#33

--- org.custusx.fraxinus.core.state/cxFraxinusSegmentations.cpp
+++ org.custusx.fraxinus.core.state/cxFraxinusSegmentations.cpp
@@ -590,9 +590,8 @@ QStringList FraxinusSegmentations::getRaidionicsOutputClasses(bool startTimers)
 	if(mSegmentAirways && !mServices->patient()->getData<Mesh>(otAIRWAYS_CENTERLINES) && !mAirwaysProcessed)
 	{
 		mActiveTimerWidget = mAirwaysTimerWidget;
+		retval << enum2string(otLUNGS);
 		retval << enum2string(otAIRWAYS);
-		if(!mServices->patient()->getData<Mesh>(otLUNGS))
-			retval << enum2string(otLUNGS);
 		if(startTimers)
 			mAirwaysTimerWidget->start();
 	}
