a76405b63736ba0b943185ff39cc1c1db70c4598
Ole Vegard Solberg <olevegard.solberg@sintef.no>
2026-04-28 10:48:41 +0200

    Add org.custusx.core.tracking.system.ndi plugin. CustusX#30

--- script/cxsetup/cxPrivateComponentAssembly.py
+++ script/cxsetup/cxPrivateComponentAssembly.py
@@ -42,6 +42,7 @@ class LibraryAssembly(cx.build.cxComponentAssembly.LibraryAssembly):
         self.addComponent(cxPrivateComponents.Fraxinus())
         self.addComponent(cxPrivateComponents.org_custusx_fraxinus_tracking())
         self.addComponent(cxPrivateComponents.org_custusx_ussimulator())
+        self.addComponent(cxPrivateComponents.org_custusx_core_tracking_system_ndi())
 	#self.addComponent(cxPrivateComponents.thoraxCTdata())
         self.libraries.remove(self.custusx)
         self.addComponent(self.custusx)
